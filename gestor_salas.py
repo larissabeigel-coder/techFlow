dbe5ea2c5079b283b189e118c50b455bb20fe741
larissabeigel-coder <larissabeigel@gmail.com>
2026-08-31 14:24:11 +0000

    Etapa dados cvs

--- gestor_salas.py
+++ gestor_salas.py
@@ -8,3 +8,15 @@ projetos_ativos =["App Mobile","Signal Found"]
 
 print("STARTUP:",startups["nome"],"Fase:",startups["etapa"])
 print("Projeto em Andamento:",projetos_ativos[0],"inicio",startups["inicio"])
+
+#Etapa2 - Mapeamento de Salas 
+#Sala Ocupada = 1 e Sala Livre =0
+salas = [
+    [1,0],
+    [0,1]
+]
+print("Status da sala A1", salas[0][0])
+print("Status da Sala A2", salas[0][1])
+print("Status da Sala B1", salas[1][0])
+print("Status da Sala B2",salas[1][1])
+print("Classificação de Status: 1= Ocupado e 0 = Livre")
