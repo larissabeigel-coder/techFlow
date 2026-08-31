e2ce6bec5b83ef7b9f27e330fc92a0495a75290d
larissabeigel-coder <larissabeigel@gmail.com>
2026-08-31 13:14:12 +0000

    Etapa de Cadastro

--- gestor_salas.py
+++ gestor_salas.py
@@ -1,1 +1,10 @@
+#Etapa1 - Criação da empresa e os projetos 
+startups = {
+    "nome": "Flare Solution",
+    "etapa": "Aceleração",
+    "inicio": "Abril de 2026"
+}
+projetos_ativos =["App Mobile","Signal Found"]
 
+print("STARTUP:",startups["nome"],"Fase:",startups["etapa"])
+print("Projeto em Andamento:",projetos_ativos[0],"inicio",startups["inicio"])
